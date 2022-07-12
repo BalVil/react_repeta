6701644c2361fa546efd4f707fca04e8baa1fdd7
BalVil <balutavili@gmail.com>
2022-07-12 04:41:50 +0300

    start, preparation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { ToastContainer } from 'react-toastify';
+// import PokemonForm from './components/PokemonForm';
+// import PokemonInfo from './components/PokemonInfo';
+
+export default class App extends Component {
+  state = {
+    pokemon: null,
+  };
+
+  componentDidMount() {
+    fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+      .then(res => res.json())
+      .then(console.log);
+  }
+  render() {
+    return (
+      <div style={{ maxWidth: 1170, margin: '0 auto', padding: 20 }}>
+        {this.state.pokemon && <div>Будет покемон тут</div>}
+        {/* <PokemonForm onSubmit={this.handleFormSubmit} />
+        <PokemonInfo pokemonName={this.state.pokemonName} /> */}
+        {/* <ToastContainer autoClose={3000} /> */}
+      </div>
+    );
+  }
+}
